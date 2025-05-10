6aae1af7e26375910c97a2b9dc36b533e098ee63
ocsan <ocsana.ekb@gmail.com>
2025-05-10 19:51:03 +0500

    дз 11 скорректированно

--- src/main/java/ru/otus/java/basic/homeworks/homework11/Main.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework11/Main.java
@@ -1,6 +1,6 @@
 package ru.otus.java.basic.homeworks.homework11;
 
-import ru.otus.java.basic.homeworks.homework11.Animals.*;
+import ru.otus.java.basic.homeworks.homework11.animals.*;
 
 import java.util.Scanner;
 
@@ -10,56 +10,14 @@ public class Main {
         Dog dog = new Dog("Барбос", 2, 1, 100);
         Horse horse = new Horse("Луна", 5, 2, 100);
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("\nВведите животное из доступных: кошка, собака, лошадь, никого.");
-            String chooseAnimal = scanner.next().trim().toLowerCase();
-            Animal current = null;
-            switch (chooseAnimal) {
-                case "кошка":
-                    current = cat;
-                    break;
-                case "собака":
-                    current = dog;
-                    break;
-                case "лошадь":
-                    current = horse;
-                    break;
-                case "никого":
-                    return;
-                default:
-                    System.out.println("\nНеверный ввод.\nПопробуйте еще раз:");
-                    continue;
-            }
-
-            boolean forward = true;
-        while (forward) {
-            System.out.println("\nВведите действие из доступных: бежать, плыть, инфо, выносливость, вернуться, выйти.");
-            String chooseAction = scanner.next().trim().toLowerCase();
-            switch (chooseAction) {
-                case "бежать":
-                    current.run();
-                    break;
-                case "плыть":
-                    current.swim();
-                    break;
-                case "инфо":
-                    current.info();
-                    break;
-                case "выносливость":
-                    current.takeStamina();
-                    break;
-                case "вернуться":
-                    forward = false;
-                    break;
-                case "выйти":
-                    return;
-                default:
-                    System.out.println("\nНеверный ввод.\nПопробуйте еще раз:");
-                    continue;
-                }
-            }
-        }
+        cat.run(10);
+        dog.run(10);
+        horse.run(10);
+        cat.swim(10);
+        dog.swim(10);
+        horse.swim(10);
+        cat.info();
+        dog.info();
+        horse.info();
     }
 }
