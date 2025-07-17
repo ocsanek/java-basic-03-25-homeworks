b65518ea1153b924c897fc77abed92026d1c5f1f
ocsan <ocsana.ekb@gmail.com>
2025-07-17 11:39:01 +0500

    дз 26 выполнено

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework26/entity/Role.java
@@ -0,0 +1,53 @@
+package ru.otus.java.basic.homeworks.homework26.entity;
+
+import java.util.Objects;
+
+public class Role {
+    private int id;
+    private String name;
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
